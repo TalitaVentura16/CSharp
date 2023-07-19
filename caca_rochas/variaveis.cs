6bc4b2d2dad3dba3e20e62d114ac46c07e2768b1
Talitaventura16 <Talitaventura16@outlook.com>
2023-07-19 17:24:49 -0300

    Adicionei a função de nome

--- caca_rochas/variaveis.cs
+++ caca_rochas/variaveis.cs
@@ -13,6 +13,28 @@ class Jogo
     {
         Jogo jogo = new Jogo();
         jogo.MensagemBoasVindas();
+
+        Console.WriteLine("Bem-vindo ao mundo das rochas, meu destemido guerreiro!");
+        Console.Write("Qual o seu nome? ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Agora, informe o seu gênero: guerreiro, guerreira ou guerreire.");
+        string genero = Console.ReadLine().ToLower();
+
+        switch (genero)
+        {
+            case "guerreiro":
+                Console.WriteLine($"Bem-vindo, guerreiro {nome}!");
+                break;
+            case "guerreira":
+                Console.WriteLine($"Bem-vinda, guerreira {nome}!");
+                break;
+            case "guerreire":
+                Console.WriteLine($"Bem-vinde, guerreire {nome}!");
+                break;
+            default:
+                Console.WriteLine($"Gênero não reconhecido para {nome}.");
+                break;
+        }
     }
 }
-
