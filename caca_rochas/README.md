fe7e31b0111a92deebf9ae2d94dc9ae9090e6ef1
Talitaventura16 <Talitaventura16@outlook.com>
2024-01-10 16:48:21 -0300

    Corrigi o readme e alterei algumas palavras.

--- caca_rochas/README.md
+++ caca_rochas/README.md
@@ -1,7 +1,30 @@
-# Caça Rochas
+#Caça Rochas: Um mágico sobre Geologia e Geomorfologia em C# ⛰️✨
 
-Bem-vindo ao aça-Rochas, a ideia desse programa é criar um jogo em C# sobre rochas, do modo a aprender a linguagem e ao mesmo tempo compartilhar curiosidades sobre geologia e geomorfologia.
+Bem-vindo ao Caça-Rochas! A ideia deste programa é desenvolver um jogo em C# sobre rochas, permitindo a aprendizagem da linguagem e compartilhando curiosidades sobre geologia e geomorfologia.
+Informações Importantes
 
-**Informações Importantes**
+**Sobre o Projeto 🤔**
 
+"Caça Rochas" foi criado como uma ferramenta de aprendizagem em C#. Proporciona uma forma envolvente de explorar conhecimentos sobre rochas, geologia e geomorfologia.
 
+**Status do Projeto 🔭 **
+
+Este é um projeto inicial em C#. Pode conter imperfeições, mas estou comprometido em melhorar. Todas as contribuições construtivas são bem-vindas. 🚀🌌
+
+**Como Contribuir 🔎**
+
+Se deseja colaborar:
+
+    Faça um fork deste repositório.
+    Crie um branch para sua contribuição (git checkout -b sua-contribuicao).
+    Realize as alterações e comente sobre elas.
+    Faça um commit (git commit -m 'Descrição da sua contribuição').
+    Envie um pull request com detalhes das alterações.
+
+Vou avaliar as contribuições; se benéficas, serão incorporadas ao projeto principal.
+
+**Atenção!**
+
+Licença
+
+Este projeto é distribuído sob a Licença MIT, permitindo o uso, modificação e distribuição do software, desde que se mantenha o aviso de direitos autorais e a isenção de responsabilidade.
--- caca_rochas/variaveis.cs
+++ caca_rochas/variaveis.cs
@@ -19,29 +19,29 @@ class Jogo
         if (escolha == "1")
         {
             Console.WriteLine("Você escolheu a Caverna da Aventura!");
-            // Aqui você pode prosseguir com uma história de aventura.
+            // Aqui você pode prosseguir com uma historia de aventura.
             Console.WriteLine("Prepare-se para enfrentar perigos desconhecidos e desvendar mistérios emocionantes!\n");
         }
         else if (escolha == "2")
         {
             Console.WriteLine("Você escolheu a Caverna do Conhecimento!");
-            // Aqui você pode prosseguir com uma história mais focada em conhecimento e desafios intelectuais.
+            // Aqui você pode prosseguir com uma historia mais focada em conhecimento e desafios intelectuais.
             Console.WriteLine("Prepare-se para aprender conceitos geológicos complexos e enfrentar enigmas intrigantes!\n");
         }
         else
         {
             Console.WriteLine("Opção inválida. Por favor, escolha 1 ou 2 para prosseguir.");
-            EscolherCaminho(); // Caso a escolha seja inválida, chama a função novamente para nova tentativa.
+            EscolherCaminho(); // Caso a escolha seja invalida, chama a funcao novamente para nova tentativa.
         }
     }
     void ExibirSinopse()
     {
         Console.WriteLine("**\"As Crônicas da Geologia Fantástica\"**");
-        Console.WriteLine("Em um mundo mágico onde as rochas escondem segredos ancestrais e a terra pulsa com energia desconhecida, você é um destemido guerreiro em busca de conhecimento e aventura. Neste reino encantado, a Geologia é uma ciência poderosa e os minerais possuem propriedades místicas.");
-        Console.WriteLine("A jornada começa quando você descobre um antigo livro que narra a lenda das rochas mágicas, capazes de conceder habilidades inimagináveis aos seus portadores. Inspirado pela ideia de desvendar os segredos ocultos sob a superfície da terra, você parte em busca dessas pedras preciosas, enfrentando perigos e desafios pelo caminho.");
-        Console.WriteLine("Ao longo da sua jornada, você encontrará criaturas místicas da geologia, como o Gigante de Granito, o Dragão do Diamante e o Elemental da Rocha, que protegem os tesouros geológicos com fúria e astúcia. Para obter o poder das pedras mágicas, você precisará compreender as características geológicas únicas de cada região e enfrentar testes de habilidade e conhecimento.");
+        Console.WriteLine("Em um mundo mágico onde as rochas escondem segredos ancestrais e a terra pulsa com energia desconhecida, você é um destemido aventureiro em busca de conhecimento e aventura. Neste reino encantado, a Geologia é uma ciência poderosa e os minerais possuem propriedades místicas.");
+        Console.WriteLine("A jornada começa quando você descobre um antigo livro que narra a lenda das rochas mágicas, capazes de conceder habilidades inimagináveis aos seus portadores. Inspirado pela ideia de desvendar os segredos ocultos sob a superfície da Terra, você parte em busca dessas pedras preciosas, enfrentando perigos e desafios pelo caminho.");
+        Console.WriteLine("Ao longo da sua jornada, você encontrará criaturas místicas da geologia, como o Gigante de Granito, o Dragão do Diamante e o Elemental da Rocha, que protegem os tesouros geológicos com fúria e astúcia. Para obter o poder das rochas mágicas, você precisará compreender as características geológicas únicas de cada região e enfrentar testes de habilidade e conhecimento.");
         Console.WriteLine("No seu percurso, você encontrará sábios geólogos que compartilharão seu vasto conhecimento sobre a terra e suas formações. Aprenderá sobre os processos da geomorfologia, a história geológica do mundo e a influência dos minerais na magia dessa terra encantada.");
-        Console.WriteLine("Aventuras emocionantes, amizades inesperadas e descobertas surpreendentes aguardam você nas Crônicas da Geologia Fantástica. Aprenda a interpretar pistas geológicas para desvendar enigmas antigos e enfrentar desafios épicos. Prove-se como um verdadeiro guerreiro da geologia e desvende o destino místico que aguarda aqueles que entendem os segredos da terra.");
+        Console.WriteLine("Aventuras emocionantes, amizades inesperadas e descobertas surpreendentes aguardam você nas Crônicas da Geologia Fantástica. Aprenda a interpretar pistas geológicas para desvendar enigmas antigos e enfrentar desafios épicos. Prove-se como um verdadeiro aventureiro da geologia e desvende o destino místico que aguarda aqueles que entendem os segredos da Terra.");
         Console.WriteLine("Neste mundo de magia e conhecimento, apenas os mais corajosos e sábios geólogos serão capazes de alcançar o poder supremo das rochas mágicas e desvendar a verdadeira essência da Geologia Fantástica!");
     }
 
@@ -51,23 +51,20 @@ class Jogo
         Jogo jogo = new Jogo();
         jogo.MensagemBoasVindas();
 
-        Console.WriteLine("Bem-vindo ao mundo das rochas, meu destemido guerreiro!");
+        Console.WriteLine("Bem-vindo ao mundo das rochas, meu destemido aventureiro!");
         Console.Write("Qual o seu nome? ");
         string nome = Console.ReadLine();
 
-        Console.WriteLine("Agora, informe o seu gênero: guerreiro, guerreira ou guerreire.");
+        Console.WriteLine("Agora, informe o seu gênero: 1 - aventureiro, 2 - aventureira.");
         string genero = Console.ReadLine().ToLower();
 
         switch (genero)
         {
-            case "guerreiro":
-                Console.WriteLine($"Bem-vindo, guerreiro {nome}!");
+            case "1":
+                Console.WriteLine($"Bem-vindo {nome}!");
                 break;
-            case "guerreira":
-                Console.WriteLine($"Bem-vinda, guerreira {nome}!");
-                break;
-            case "guerreire":
-                Console.WriteLine($"Bem-vinde, guerreire {nome}!");
+            case "2":
+                Console.WriteLine($"Bem-vinda {nome}!");
                 break;
             default:
                 Console.WriteLine($"Gênero não reconhecido para {nome}.");
